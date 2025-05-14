b9fa48319f8d2bc4904ab961e5631448f9a8397e
backas36 <backas36@gmail.com>
2025-05-15 00:02:13 +0800

    feat: wip bitcoin feature dropdown price unit

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,13 +1,14 @@
 import 'package:ashi_playground/feature/bmi/models/bmi_result.dart';
 import 'package:go_router/go_router.dart';
 
+import '../feature/bitcoin_ticker/price_screen.dart';
 import '../feature/bmi/bmi_screen.dart';
 import '../feature/bmi/result_page.dart';
 import '../feature/timer/countdown_screen.dart';
 import '../feature/timer/timer_screen.dart';
 
 final router = GoRouter(
-  initialLocation: "/countdown",
+  initialLocation: "/bitcoin",
   routes: [
     GoRoute(path: '/', builder: (context, state) => const BmiScreen()),
     GoRoute(
@@ -26,5 +27,6 @@ final router = GoRouter(
       path: "/countdown",
       builder: (context, state) => const CountdownScreen(),
     ),
+    GoRoute(path: "/bitcoin", builder: (context, state) => const PriceScreen()),
   ],
 );
